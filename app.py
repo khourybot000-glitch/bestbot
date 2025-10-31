0e05d485843ed550041288a56bb2dedfc2317831
khourybot000-glitch <khourybot000@gmail.com>
2025-10-31 11:04:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -699,6 +699,6 @@ def logout():
     return redirect(url_for('auth_page'))
 
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
