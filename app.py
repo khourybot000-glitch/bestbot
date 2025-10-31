626872ae8e556b3b51539cf58623010e29163a6f
khourybot000-glitch <khourybot000@gmail.com>
2025-10-31 17:18:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -436,7 +436,7 @@ def bot_core_logic(email, token, stake, tp):
 # ==========================================================
 # FLASK APP SETUP AND ROUTES
 # ==========================================================
-app = Flask(_name_) # ⬅ تم تصحيح هذا السطر
+app = Flask(__name__) # ⬅ تم تصحيح هذا السطر
 app.config['SECRET_KEY'] = os.environ.get('SESSION_SECRET_KEY', 'VERY_STRONG_SECRET_KEY_RENDER_BOT')
 app.config['SESSION_PERMANENT'] = False 
 
@@ -699,6 +699,6 @@ def logout():
     return redirect(url_for('auth_page'))
 
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
