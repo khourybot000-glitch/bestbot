06f96263b4bf6370c237d464228ee3013f97591f
khourybot000-glitch <khourybot000@gmail.com>
2025-12-02 12:31:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -775,4 +775,4 @@ if __name__ == '__main__':
             f.write('{}')
             
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=
+    app.run(host='0.0.0.0', port=port, debug=False
